f216d94231d5c83ef556c403077d0b50b23e91d8
Ajinath-Game <gameajinath4@gmail.com>
2025-10-02 10:57:42 +0530

    Learn about Timeline

---
+++ DAY3/script.js
@@ -0,0 +1,83 @@
+let tl = gsap.timeline();
+
+tl.from("nav",{
+    y: "-100%",
+    duration: 0.5,
+    opacity: 0,
+   
+})
+
+tl.from(".img-con",{
+    y: "-10%",
+    duration: 0.5,
+    opacity: 0,
+    
+})
+tl.from(".text h1",{
+    y: "-10%",
+    duration: 0.5,
+    opacity: 0,
+    
+})
+tl.from(".text p",{
+    y: "-10%",
+    duration: 0.5,
+    opacity: 0,
+    
+})
+
+tl.from(".img1",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part1")
+tl.from(".img2",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part2")
+tl.from(".img3",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part1")
+tl.from(".img4",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part1")
+tl.from(".img5",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part2")
+tl.from(".img6",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part2")
+tl.from(".img7",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part2")
+tl.from(".img8",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part2")
+tl.from(".img9",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part1")
+tl.from(".img10",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part1")
+tl.from(".img11",{
+    y: "100%",
+    duration: 0.3,
+    opacity: 0,
+},"part1")
